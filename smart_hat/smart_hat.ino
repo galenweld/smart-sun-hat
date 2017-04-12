9c07781c47d236ee6054b555dbac839060c2418a
Kevin Guo <kg344@cornell.edu>
2017-04-12 15:32:26 -0400

    Initial code, needs to be tested

--- smart_hat/smart_hat.ino
+++ smart_hat/smart_hat.ino
@@ -6,7 +6,7 @@
 #include "Adafruit_SI1145.h"
 #include <Adafruit_NeoPixel.h>
 
-int temp;
+float temp;
 float visLight;
 float uvLight;
 
@@ -19,12 +19,14 @@ const int tempSensor = 12; //temperature sensor to pin 12
 Adafruit_SI1145 uv = Adafruit_SI1145();
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, led, NEO_GRB + NEO_KHZ800);
 
-  uint32_t red = strip.Color(255, 0, 0);
-  uint32_t green = strip.Color(0, 255, 255);
-  uint32_t blue = strip.Color(0, 0, 255);
-  uint32_t purple = strip.Color(255, 0, 255);
-  uint32_t white = strip.Color(255, 255, 255);
+uint32_t red = strip.Color(255, 0, 0);
+uint32_t purple = strip.Color(255, 0, 255);
+uint32_t blue = strip.Color(0, 0, 255);
 
+uint32_t green = strip.Color(0, 255, 0);
+uint32_t yellow = strip.Color(255, 255, 0);
+uint32_t orange = strip.Color(255, 144, 0);
+  
 void setup() {
   //Begins serial communcation with temperature sensor, photoresistor
   Serial.begin(9600);
@@ -56,36 +58,53 @@ void loop() {
 
   //temperature sensor
   temp = analogRead(tempSensorPin);
-  degreesC = sensorValue/2.046 - 50.0;
+  temp = temp/2.046 - 50.0;
   
   //==============OUTPUTS==============
   //buzzer
-  tone(buzzer, 1000);
-  delay(1000); //waits a second
-
-  //led output for the first led, will indicate 
-  if(visLight >= ){
-  
+  if(uvLight >= 5){
+    tone(buzzer, 1000);
+    delay(3000);
   }
-  else if(visLight >= ){
-    
+  else if(uvLight >= 2){
+    tone(buzzer, 1000);
+    delay(1500);
   }
   else{
-  
+    tone(buzzer, 1000);
+    delay(750);
   }
+  noTone(buzzer);
+  delay(1000);
   
-  //led output for 
-  if(uvLight >= ){
-    strip.setPixelColor()
+  //led output for the first led, will indicate temperature
+  //from blue at low temps to red at hot temps
+  if(temp >= 27){
+    strip.setPixelColor(0, red);
+    strip.show();
   }
-  else if(uvLight >=){
-  
+  else if(temp >= 22){
+    strip.setPixelColor(0, purple);
+    strip.show();
   }
   else{
-  
+    strip.setPixelColor(0, blue);
+    strip.show();
   }
   
-  //analogWrite(led, analogRead(photoResistor)/4);
-  //delay(1000); //waits a second
-
+  //led output for the second led, will indicate visible light
+  //from green at low light to orange at high light
+` if(visLight >= 220){
+    strip.setPixelColor(1, orange);
+    strip.show();
+  }
+  else if(visLight >= 200){
+    strip.setPixelColor(1, yellow);
+    strip.show();
+  }
+  else{
+    strip.setPixelColor(1, green);
+    strip.show();
+  }
+  delay(10);
 }
