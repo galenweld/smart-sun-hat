916cc1ccfec41c9305507445419581fdeb8d9990
Galen Weld <galen.weld@gmail.com>
2017-04-12 14:20:54 -0400

    created initial file for project

---
+++ smart_hat/smart_hat.ino
@@ -0,0 +1,14 @@
+// Smart UV Sensing Hat project for Cornell INFO 4120
+// Kevin Guo (kg344) and Galen Weld (gcw33)
+// April 2017
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
